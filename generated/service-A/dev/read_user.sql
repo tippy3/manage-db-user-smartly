e1b766d5e60a181340cf306028e92b79636deddf
tippy3 <26988793+tippy3@users.noreply.github.com>
2024-02-03 02:20:52 +0900

    fix ip

--- generated/service-A/dev/read_user.sql
+++ generated/service-A/dev/read_user.sql
@@ -1,17 +1,17 @@
-CREATE USER 'akane.kurokawa'@'173.18.0.0/255.255.255.0' IDENTIFIED WITH AWSAuthenticationPlugin AS 'RDS';
-GRANT SELECT ON service_a.* TO 'akane.kurokawa'@'173.18.0.0/255.255.255.0';
+CREATE USER 'akane.kurokawa'@'172.18.0.0/255.255.255.0' IDENTIFIED WITH AWSAuthenticationPlugin AS 'RDS';
+GRANT SELECT ON service_a.* TO 'akane.kurokawa'@'172.18.0.0/255.255.255.0';
 
-CREATE USER 'akane.kurokawa'@'173.18.1.0/255.255.255.0' IDENTIFIED WITH AWSAuthenticationPlugin AS 'RDS';
-GRANT SELECT ON service_a.* TO 'akane.kurokawa'@'173.18.1.0/255.255.255.0';
+CREATE USER 'akane.kurokawa'@'172.18.1.0/255.255.255.0' IDENTIFIED WITH AWSAuthenticationPlugin AS 'RDS';
+GRANT SELECT ON service_a.* TO 'akane.kurokawa'@'172.18.1.0/255.255.255.0';
 
-CREATE USER 'akane.kurokawa'@'173.18.2.0/255.255.255.0' IDENTIFIED WITH AWSAuthenticationPlugin AS 'RDS';
-GRANT SELECT ON service_a.* TO 'akane.kurokawa'@'173.18.2.0/255.255.255.0';
+CREATE USER 'akane.kurokawa'@'172.18.2.0/255.255.255.0' IDENTIFIED WITH AWSAuthenticationPlugin AS 'RDS';
+GRANT SELECT ON service_a.* TO 'akane.kurokawa'@'172.18.2.0/255.255.255.0';
 
-CREATE USER 'goro.amamiya'@'173.18.0.0/255.255.255.0' IDENTIFIED WITH AWSAuthenticationPlugin AS 'RDS';
-GRANT SELECT ON service_a.* TO 'goro.amamiya'@'173.18.0.0/255.255.255.0';
+CREATE USER 'goro.amamiya'@'172.18.0.0/255.255.255.0' IDENTIFIED WITH AWSAuthenticationPlugin AS 'RDS';
+GRANT SELECT ON service_a.* TO 'goro.amamiya'@'172.18.0.0/255.255.255.0';
 
-CREATE USER 'goro.amamiya'@'173.18.1.0/255.255.255.0' IDENTIFIED WITH AWSAuthenticationPlugin AS 'RDS';
-GRANT SELECT ON service_a.* TO 'goro.amamiya'@'173.18.1.0/255.255.255.0';
+CREATE USER 'goro.amamiya'@'172.18.1.0/255.255.255.0' IDENTIFIED WITH AWSAuthenticationPlugin AS 'RDS';
+GRANT SELECT ON service_a.* TO 'goro.amamiya'@'172.18.1.0/255.255.255.0';
 
-CREATE USER 'goro.amamiya'@'173.18.2.0/255.255.255.0' IDENTIFIED WITH AWSAuthenticationPlugin AS 'RDS';
-GRANT SELECT ON service_a.* TO 'goro.amamiya'@'173.18.2.0/255.255.255.0';
+CREATE USER 'goro.amamiya'@'172.18.2.0/255.255.255.0' IDENTIFIED WITH AWSAuthenticationPlugin AS 'RDS';
+GRANT SELECT ON service_a.* TO 'goro.amamiya'@'172.18.2.0/255.255.255.0';
