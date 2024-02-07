fe8e7fa71b0059c2d912e70b1c8fb4406a702efd
tippy3 <26988793+tippy3@users.noreply.github.com>
2024-02-07 22:44:48 +0900

    remove unnecessary semicolon

--- generated/service-A/prd/admin_user.sql
+++ generated/service-A/prd/admin_user.sql
@@ -1,29 +1,29 @@
-CREATE USER 'ai.hoshino'@'172.16.0.0/255.255.255.0' IDENTIFIED BY 'password';;
+CREATE USER 'ai.hoshino'@'172.16.0.0/255.255.255.0' IDENTIFIED BY 'password';
 GRANT ALL ON *.* TO 'ai.hoshino'@'172.16.0.0/255.255.255.0';
 GRANT PROCESS ON *.* TO 'ai.hoshino'@'172.16.0.0/255.255.255.0';
 GRANT ALL PRIVILEGES ON sys.* TO 'ai.hoshino'@'172.16.0.0/255.255.255.0';
 
-CREATE USER 'ai.hoshino'@'172.16.1.0/255.255.255.0' IDENTIFIED BY 'password';;
+CREATE USER 'ai.hoshino'@'172.16.1.0/255.255.255.0' IDENTIFIED BY 'password';
 GRANT ALL ON *.* TO 'ai.hoshino'@'172.16.1.0/255.255.255.0';
 GRANT PROCESS ON *.* TO 'ai.hoshino'@'172.16.1.0/255.255.255.0';
 GRANT ALL PRIVILEGES ON sys.* TO 'ai.hoshino'@'172.16.1.0/255.255.255.0';
 
-CREATE USER 'ai.hoshino'@'172.16.2.0/255.255.255.0' IDENTIFIED BY 'password';;
+CREATE USER 'ai.hoshino'@'172.16.2.0/255.255.255.0' IDENTIFIED BY 'password';
 GRANT ALL ON *.* TO 'ai.hoshino'@'172.16.2.0/255.255.255.0';
 GRANT PROCESS ON *.* TO 'ai.hoshino'@'172.16.2.0/255.255.255.0';
 GRANT ALL PRIVILEGES ON sys.* TO 'ai.hoshino'@'172.16.2.0/255.255.255.0';
 
-CREATE USER 'kana.arima'@'172.16.0.0/255.255.255.0' IDENTIFIED BY 'password';;
+CREATE USER 'kana.arima'@'172.16.0.0/255.255.255.0' IDENTIFIED BY 'password';
 GRANT ALL ON *.* TO 'kana.arima'@'172.16.0.0/255.255.255.0';
 GRANT PROCESS ON *.* TO 'kana.arima'@'172.16.0.0/255.255.255.0';
 GRANT ALL PRIVILEGES ON sys.* TO 'kana.arima'@'172.16.0.0/255.255.255.0';
 
-CREATE USER 'kana.arima'@'172.16.1.0/255.255.255.0' IDENTIFIED BY 'password';;
+CREATE USER 'kana.arima'@'172.16.1.0/255.255.255.0' IDENTIFIED BY 'password';
 GRANT ALL ON *.* TO 'kana.arima'@'172.16.1.0/255.255.255.0';
 GRANT PROCESS ON *.* TO 'kana.arima'@'172.16.1.0/255.255.255.0';
 GRANT ALL PRIVILEGES ON sys.* TO 'kana.arima'@'172.16.1.0/255.255.255.0';
 
-CREATE USER 'kana.arima'@'172.16.2.0/255.255.255.0' IDENTIFIED BY 'password';;
+CREATE USER 'kana.arima'@'172.16.2.0/255.255.255.0' IDENTIFIED BY 'password';
 GRANT ALL ON *.* TO 'kana.arima'@'172.16.2.0/255.255.255.0';
 GRANT PROCESS ON *.* TO 'kana.arima'@'172.16.2.0/255.255.255.0';
 GRANT ALL PRIVILEGES ON sys.* TO 'kana.arima'@'172.16.2.0/255.255.255.0';
